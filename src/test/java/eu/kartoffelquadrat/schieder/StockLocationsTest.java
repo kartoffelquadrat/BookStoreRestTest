4a027fd61446499affc3b067d897fe53a0c0e83a
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2022-01-18 15:10:31 -0500

    Added test for looking up amount copies at specific location / book

--- src/test/java/eu/kartoffelquadrat/schieder/StockLocationsTest.java
+++ src/test/java/eu/kartoffelquadrat/schieder/StockLocationsTest.java
@@ -28,7 +28,8 @@ public class StockLocationsTest extends RestTestUtils {
     }
 
     /**
-     * Verifies the default stock amount for book 9780739360385 (Harry Potter) is "4".
+     * Verifies the default stock amount (retrieved as bundle with all books) lists the number of copies for
+     * 9780739360385 (Harry Potter) as "4".
      *
      * @throws UnirestException
      */
@@ -41,10 +42,29 @@ public class StockLocationsTest extends RestTestUtils {
         HttpResponse<String> stockAmount = Unirest.get(getServiceURL("/stocklocations/" + location)).asString();
         verifyOk(stockAmount);
 
-        // Verify intial stock count for default book
+        // Verify initial stock count for default book
         String[] body = stockAmount.getBody().split(":");
         assert body[0].contains("9780739360385");
         assert body[1].contains("4");
+    }
+
+    /**
+     * Verifies the default stock amount for book 9780739360385 (Harry Potter) is "4".
+     *
+     * @throws UnirestException
+     */
+    @Test
+    public void testStocklocationsStocklocationIsbnsGet() throws UnirestException {
+
+        String location = "Lyon";
+        String isbn = "9780739360385";
+
+        // Try to retrieve comments for default book
+        HttpResponse<String> stockAmount = Unirest.get(getServiceURL("/stocklocations/" + location + "/" + isbn)).asString();
+        verifyOk(stockAmount);
+
+        // Verify initial stock count for default book
+        assert(stockAmount.getBody().equals("4"));
     }
 
 
